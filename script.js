e78fdecd63a6c24946d507cf541973a15aa86121
Kucciii <kucciyeager@gmail.com>
2023-06-29 15:40:30 +0530

    minor issue fixed

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function printTasks() {
           <input id="bordered-checkbox-${tasks.indexOf(task)+1}" type="checkbox" value="" name="bordered-checkbox"
             class="checkTask w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
           <label for="bordered-checkbox-${tasks.indexOf(task)+1}"
-            class="w-full py-4 pr-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">${task.taskMsg}</label>
+            class="w-full py-4 pr-4 ml-2 text-sm font-medium text-gray-300 dark:text-gray-300">${task.taskMsg}</label>
         </div>
       </div>`
     })
